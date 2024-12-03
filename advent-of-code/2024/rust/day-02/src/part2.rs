5456a02d2ff208086ce9aca1c3874842977ad338
yudhiesh <yudhiesh1997@gmail.com>
2024-12-03 21:57:58 +0800

    feat: add day 2 part 2

--- advent-of-code/2024/rust/day-02/src/part2.rs
+++ advent-of-code/2024/rust/day-02/src/part2.rs
@@ -1,6 +1,52 @@
+use super::part1::*;
+
+pub fn safe_level(levels: &Vec<u32>) -> miette::Result<bool> {
+    let is_monotonic = check_monotonicity(levels)?;
+    let is_difference_in_range = check_adjacent_levels_difference(levels)?;
+    let is_safe = is_monotonic && is_difference_in_range;
+    Ok(is_safe)
+}
+
+pub fn combinations_without_replacement(levels: &Vec<u32>) -> miette::Result<Vec<Vec<u32>>> {
+    Ok((0..levels.len())
+        .map(|i| {
+            levels
+                .iter()
+                .enumerate()
+                .filter(|(j, _)| *j != i)
+                .map(|(_, &x)| x)
+                .collect()
+        })
+        .collect())
+}
+
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    Ok("4".to_string())
+    let mut safe_reports: u32 = 0;
+    let parsed_levels: Vec<Vec<u32>> = _input
+        .lines()
+        .map(|levels| {
+            levels
+                .split_whitespace()
+                .map(|level| level.parse::<u32>().unwrap())
+                .collect()
+        })
+        .collect();
+    for levels in parsed_levels.iter() {
+        let is_safe = safe_level(levels)?;
+        if is_safe {
+            safe_reports += 1
+        } else {
+            let combinations = combinations_without_replacement(levels)?;
+            if combinations
+                .iter()
+                .any(|combination| safe_level(combination).unwrap_or(false))
+            {
+                safe_reports += 1
+            }
+        }
+    }
+    Ok(safe_reports.to_string())
 }
 
 #[cfg(test)]
