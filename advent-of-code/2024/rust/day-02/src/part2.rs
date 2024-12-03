2d04a1ce96d92d128ff38b890e6d762c1abf485e
Yudhiesh <yudhiesh@trontalgroup.com>
2024-12-03 14:22:36 +0800

    feat: add part 2 day 2 base data

--- advent-of-code/2024/rust/day-02/src/part2.rs
+++ advent-of-code/2024/rust/day-02/src/part2.rs
@@ -1,6 +1,6 @@
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+    Ok("4".to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +9,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("4", process(input)?);
         Ok(())
     }
 }
